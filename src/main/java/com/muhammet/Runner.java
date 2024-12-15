40ebb3420b1340afd907b761fcf98c09e3c5cb6a
bilgejava6 <bilgejava6@gmail.com>
2024-12-15 10:16:03 +0300

    Initial commit

---
+++ src/main/java/com/muhammet/Runner.java
@@ -0,0 +1,24 @@
+package com.muhammet;
+
+public class Runner {
+    public static void main(String[] args) {
+        /**
+         * Input / Output -> Giriş Çıkış işlemleri
+         * File IO, bir dosyayı okumak ya da Yazmak.
+         * Bu işlemleri yaparken işlerimizi kolaylaştırmak için kullanmakta olduğumuz Objeler bu
+         * okuma yazma işlemlerinde de aktif kullanılmaktadır. Bu nedenle nesne dediğimiz bilgiler
+         * RAM üzerinde bulunan adresler ve verilerdir. Bunları bir dosyaya aktarılabilmesi için
+         * özel bir şekilde paketlenmesi işimizi kolaylaştırı. İşte burada Serilizable kavramı
+         * karşımıza çıkar.
+         * Serializable, serileştime yapılacak olan sınıfı işaretlemek için kullanılan
+         * boş bir arayüzdür.
+         * marker interface -> içerisinde herhangi bir method ve props olmayan sadece işaretleme
+         * yapmak için tasarlanmış interface lere denilir.
+         * Neden Serileştirme yapılır?
+         * 1. Nesneyi Saklamak (new Musteri() - Object): Nesneleri bir dosyaya ya da veritabına kaydetmek.
+         * 2. Network(Ağ) üzerinden iletim: Nesneleri bir bilgisayardan diğerine ağ üzserinden iletmek için.
+         * 3. Önbelleklemek (RAM de bekletme): nesnelerin RAM üzerinde tutulması.
+         *
+         */
+    }
+}
